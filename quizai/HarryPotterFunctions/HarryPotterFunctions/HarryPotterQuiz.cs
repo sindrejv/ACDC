7c75ff177361c5cdf9ad0335bf13b0157bf4e8d5
Vegard Strand <vegard.strand@prosesspilotene.no>
2025-01-23 14:45:23 +0100

    Small changes

--- quizai/HarryPotterFunctions/HarryPotterFunctions/HarryPotterQuiz.cs
+++ quizai/HarryPotterFunctions/HarryPotterFunctions/HarryPotterQuiz.cs
@@ -15,21 +15,10 @@ namespace HarryPotterFunctions
     {
         [FunctionName("HarryPotterQuiz")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            /*
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-            */
 
             var openAi = new AzureOpenAi();
 
